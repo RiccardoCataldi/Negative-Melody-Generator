7182d1fcfc39da5caed4bb21367f36e034ac2a65
RiccardoCataldi <74554002+RiccardoCataldi@users.noreply.github.com>
2020-12-23 17:23:35 +0100

    Add files via upload

---
+++ Negative Melody.py
@@ -0,0 +1,48 @@
+from music21 import *
+
+environment.set('musescoreDirectPNGPath', 'C:/Program Files/MuseScore 3/bin/MuseScore3.exe')
+environment.set("musicxmlPath", 'C:/Program Files/MuseScore 3/bin/MuseScore3.exe')
+
+notelis = input('insert melody(each note needs to be separeted by space): ')
+notelist = notelis.split()
+d = {}
+for x in range(len(notelist)):
+    d["note{0}".format(x)] = notelist[x]
+
+streamlist = list(d.values())
+print(streamlist)
+stream = stream.Stream()
+streamtonote = []
+for elem in streamlist:
+    streamtonote.append(note.Note(elem))
+
+for i in streamtonote:
+    stream.append(i)
+
+stream.show('midi')
+
+newlist = []
+
+
+for i in range(len(streamtonote)):
+    Interval = interval.Interval(noteStart=stream[0], noteEnd=stream[i])
+    for i in range(89):
+        if Interval == interval.Interval(i):
+            x1 = stream[0].transpose(-i)
+            newlist.append(x1)
+        elif Interval == interval.Interval(-i):
+            x1 = stream[0].transpose(+i)
+            newlist.append(x1)
+        else:
+            pass
+
+
+
+print(newlist)
+
+from music21 import stream
+s = stream.Stream()
+for i in newlist:
+    s.append(i)
+
+s.show('midi')
